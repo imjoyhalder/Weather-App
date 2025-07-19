fa0d9a3f453ec3e7e0b6d9ab1bd55e55bb179c14
imjoyhalder <joyhalder00113355@gmail.com>
2025-07-20 05:17:21 +0600

    api issue face but not solve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 
 import Navbar from "./components/Navbar";
 
+//https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3efbd03c278fab207e0ac146012884e2
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
