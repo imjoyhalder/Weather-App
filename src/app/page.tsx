39c0173cc4d99c4175c555eadddfd3a19c982c84
imjoyhalder <joyhalder00113355@gmail.com>
2025-07-20 14:56:22 +0600

    Api problem fixed for weather data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,91 @@
+'use client';
 
+import { useQuery } from "@tanstack/react-query";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
-//https://api.openweathermap.org/data/2.5/forecast?q=pune&appid=3efbd03c278fab207e0ac146012884e2
+
+
+export interface WeatherForecast {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  rain?: {
+    '3h': number;
+  };
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
+export interface ForecastApiResponse {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherForecast[];
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
+}
+
+
 export default function Home() {
+  
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+      );
+      return data;
+    }
+  });
+  console.log(data?.city?.name)
+
+  if (isPending) return <div className="flex items-center min-h-screen justify-center">
+    <p className="animate-bounce">Loading...</p>
+  </div>;
+  if (error) return <div>Error occurred!</div>;
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar></Navbar>
+      
     </div>
   );
 }
